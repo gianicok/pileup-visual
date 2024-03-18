1e6cf8b0c0dcf2ff698ce5b1a957dbd728e97974
gianicok <kostagianicos@gmail.com>
2024-03-18 16:46:11 -0400

    organize into functions

--- pulse.py
+++ pulse.py
@@ -34,46 +34,55 @@ class _pulse:
     
 # ----------------------------------------------------------------------------------------------------------
 
-# params
-volt = 12
-tau = 1
-num = int(sys.argv[1])
-res = 1000
-x1 = 0
-x2 = 100
+def create_peaks(num): 
+    volt = 12
+    tau = 1e-2
+    
+    peaks = []
+    for i in range(num):
+        peaks.append(_pulse(volt,tau))
+    return peaks
+    
+def create_data(peaks):  
+    data = []
+    res = 10000
+    x1 = 0
+    x2 = 1
+    for peak in peaks:
+        peak.generate(x1,x2,res,random.uniform(0,x2))
+        data.append(peak.dataframe())
+    return data
 
-# create list of objects
-peaks = []
-for i in range(num):
-    peaks.append(_pulse(volt,tau))
+def create_pileup(data):
+    sum = 0
+    for peak in data:
+        sum += peak['y']
+    pileup = pd.DataFrame({'x': peaks[0].df['x'], 'y': sum})
+    return pileup
 
-# generate data for each
-data = []
-for peak in peaks:
-    peak.generate(x1,x2,res,random.randint(0,int(x2*0.7)))
-    data.append(peak.dataframe())
+def plotter(peaks):
+    fig, (ax1, ax2) = plt.subplots(2, 1)
 
-sum = 0
-for peak in data:
-    sum += peak['y']
-pileup = pd.DataFrame({'x': peaks[0].df['x'], 'y': sum})
+    for peak in peaks:
+        peak = peak.df
+        ax1.plot(peak['x'],peak['y'])
+
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Output (Volts)')
+    ax1.set_title('Individual Peaks')
+
+    ax2.plot(pileup['x'], pileup['y'],color='red')
+    ax2.set_xlabel('Time (s)')
+    ax2.set_ylabel('Output (Volts)')
+    ax2.set_title('Sum-Peaks "Pulse-Pileup"')
+
+    plt.tight_layout()
+    plt.show()
 
 # ----------------------------------------------------------------------------------------------------------
 
-fig, (ax1, ax2) = plt.subplots(2, 1)
+peaks = create_peaks(int(sys.argv[1]))
+data = create_data(peaks)
+pileup = create_pileup(data)
 
-for peak in peaks:
-    peak = peak.df
-    ax1.plot(peak['x'],peak['y'])
-
-ax1.set_xlabel('Time (s)')
-ax1.set_ylabel('Output (Volts)')
-ax1.set_title('Individual Peaks')
-
-ax2.plot(pileup['x'], pileup['y'],color='red')
-ax2.set_xlabel('Time (s)')
-ax2.set_ylabel('Output (Volts)')
-ax2.set_title('Sum-Peaks "Pulse-Pileup"')
-
-plt.tight_layout()
-plt.show()
+plotter(peaks)
